63e093a4900e64c70885727f3eba50af13921692
Niku Grönberg <Niku.Gronberg@lut.fi>
2020-03-27 10:27:41 +0200

    add filtering topic summaries

--- modules/analysisDetailsOutput.R
+++ modules/analysisDetailsOutput.R
@@ -1,5 +1,10 @@
 DetailSettings <- list(
-  "labelCount" = 10
+  "keywords" = 10,
+  "minKeywords" = 1,
+  "maxKeywords" = 1000,
+  "documents" = 2,
+  "minDocuments" = 0,
+  "maxDocuments" = 1000
 )
 
 analysisDetailsOutput <- function(id) {
@@ -9,7 +14,35 @@ analysisDetailsOutput <- function(id) {
     fluidRow(
       column(width = 4,
         wellPanel(
-          tags$h2("Filter")
+          tags$h2("Filter"),
+          checkboxInput(inputId = ns("orderEmotions"),
+                        label = "Sort emotion analysis",
+                        value = FALSE
+                        ),
+          numericInput(inputId = ns("numberOfKeywords"),
+                       label = "Number of keywords",
+                       value = DetailSettings$keywords,
+                       min = DetailSettings$minKeywords,
+                       max = DetailSettings$maxKeywords
+                       ),
+          numericInput(inputId = ns("showDocuments"),
+                       label = "Show top documents",
+                       value = DetailSettings$documents, 
+                       min = DetailSettings$minDocuments,
+                       max = DetailSettings$maxDocuments
+                       ),
+          checkboxInput(inputId = ns("hideKeywords"),
+                        label = "Hide keywords",
+                        value = FALSE),
+          checkboxInput(inputId = ns("hideSentiment"),
+                        label = "Hide sentiment",
+                        value = FALSE),
+          checkboxInput(inputId = ns("hideEmotions"),
+                        label = "Hide emotions",
+                        value = FALSE),
+          checkboxInput(inputId = ns("hideDocuments"),
+                        label = "Hide documents",
+                        value = FALSE)
         )
       ),
       column(width = 8,
@@ -21,49 +54,19 @@ analysisDetailsOutput <- function(id) {
 }
 
 analysisDetailsOutputFunction <- function(input, output, session, resultsr) {
-  showDocumentsInputsr <- reactive({
+  observeEvent(c(resultsr(), input$hideEmotions), {
     validate(
       need(resultsr(), message = FALSE)
     )
-    results <- resultsr()
-    
-    inputs <- list()
-    inputs <- lapply(1:results$model$settings$dim$K, function(topic) {
-      showDocumentsName <- paste0("showDocuments", topic)
-      inputs <- paste0(inputs, input[[showDocumentsName]])
-    })
-  })
-  
-  orderEmotionsr <- reactive({
-    validate(
-      need(resultsr(), message = FALSE)
-    )
-    results <- resultsr()
-    
-    inputs <- list()
-    inputs <- lapply(1:results$model$settings$dim$K, function(topic) {
-      orderName <- paste0("sortEmotions", topic)
-      inputs <- paste0(inputs, input[[orderName]])
-    })
-  })
-  
-  
-  
-  observeEvent(resultsr(), {
     results <- resultsr()
     model <- results$model
     
-    topicLabels <- labelTopics(model, n = DetailSettings$labelCount)
     removeUI(selector = "#topics >", multiple = TRUE)
     
     lapply(1:model$settings$dim$K, function(topic) {
       # Generate UI
-      ui <- generateUI(topic, model, results$topicSentiment[[topic]], session$ns)
+      ui <- generateUI(topic, model, results$topicSentiment[[topic]], session$ns, input)
       insertUI(selector = "#topics", where = "beforeEnd", ui = ui)
-      
-      # Key words
-      outputKeywords <- paste0("keywords", topic)
-      textOut <- keywordsTextFormat(topicLabels, topic)
       
       # Sentiment
       outputSentiment <- paste0("sentiment", topic)
@@ -71,28 +74,68 @@ analysisDetailsOutputFunction <- function(input, output, session, resultsr) {
       sentimentPlot <- simpleSentimentBarPlot(sentiment, plotTitle = NULL)
       
       # Outputs
-      output[[outputKeywords]] <- renderText({
-        textOut
-      })
-      
       output[[outputSentiment]] <- renderPlot({
         sentimentPlot
       }, bg = "transparent")
     })
   })
   
-  observeEvent(showDocumentsInputsr(), {
+  observeEvent(c(resultsr(), input$numberOfKeywords), {
     validate(
       need(resultsr(), message = FALSE),
-      need(showDocumentsInputsr(), message = FALSE)
+      need(is.numeric(input$numberOfKeywords), message = "Invalid number of keywords"),
+      need(input$numberOfKeywords <= DetailSettings$maxKeywords & input$numberOfKeywords >= DetailSettings$minKeywords,
+           message = "Invalid number of keywords")
     )
-    
-    lengths <- showDocumentsInputsr()
     results <- resultsr()
     model <- results$model
     
-    lapply(1:length(lengths), function(topic) {
-      documentCount <- lengths[[topic]]
+    topicLabels <- labelTopics(model, n = input$numberOfKeywords)
+    
+    lapply(1:model$settings$dim$K, function(topic) {
+      outputKeywords <- paste0("keywords", topic)
+      textOut <- keywordsTextFormat(topicLabels, topic)
+      
+      output[[outputKeywords]] <- renderUI({
+        textOut
+      })
+    })
+  })
+  
+  observeEvent(c(resultsr(), input$orderEmotions), {
+    validate(
+      need(resultsr(), message = FALSE)
+    )
+    results <- resultsr()
+    model <- results$model
+    
+    lapply(1:model$settings$dim$K, function(topic) {
+      outputEmotions <- paste0("emotion", topic)
+      emotions <- results$topicSentiment[[topic]][1:8, ]
+      emotionPlot <- sentimentBarPlot(emotions,
+                                      orderData = input$orderEmotions,
+                                      plotTitle = NULL,
+                                      transparent = TRUE)
+      
+      output[[outputEmotions]] <- renderPlot({
+        emotionPlot
+      }, bg = "transparent")
+    })
+  })
+  
+  observeEvent(c(resultsr(), input$showDocuments), {
+    validate(
+      need(resultsr(), message = FALSE),
+      need(is.numeric(input$showDocuments), message = "Invalid number of documents"),
+      need(input$numberOfKeywords <= DetailSettings$maxDocuments & input$numberOfKeywords >= DetailSettings$minDocuments,
+           message = "Invalid number of documents")
+    )
+  
+    results <- resultsr()
+    model <- results$model
+    
+    lapply(1:model$settings$dim$K, function(topic) {
+      documentCount <- input$showDocuments
       
       validate(need(documentCount, message = FALSE))
       
@@ -110,65 +153,49 @@ analysisDetailsOutputFunction <- function(input, output, session, resultsr) {
       })
     })
   })
-  
-  observeEvent(orderEmotionsr(), {
-    validate(
-      need(resultsr(), message = FALSE),
-      need(orderEmotionsr(), message = FALSE)
-    )
-    
-    orderings <- orderEmotionsr()
-    results <- resultsr()
-
-    
-    lapply(1:length(orderings), function(topic) {
-      validate(
-        need(orderings[[topic]], message = FALSE)
-      )
-
-      emotions <- results$topicSentiment[[topic]][1:8, ]
-      emotionPlot <- sentimentBarPlot(emotions, orderData = orderings[[topic]], plotTitle = NULL, transparent = TRUE)
-      
-      outputEmotion <- paste0("emotion", topic)
-      output[[outputEmotion]] <- renderPlot({
-        emotionPlot
-      }, bg = "transparent")
-    })
-  })
 }
 
-generateUI <- function(topic, model, sentiment, ns) {
+generateUI <- function(topic, model, sentiment, ns, input) {
   topicProportion <- sum(model$theta[, topic] / model$settings$dim$N)
   ui <- tagList(
     wellPanel(
       fluidRow(
-        column(width = 6,
+        column(width = 12,
           tags$h3(paste("Topic", topic)),
-          tags$p(paste0("Topic proportion ", round(topicProportion * 100, 0), "%")),
-          tags$h3("Key words"),
-          verbatimTextOutput(outputId = ns(paste0("keywords", topic)))
-        ),
-        column(width= 6,
-          tags$h3("Topic sentiment"),
-          plotOutput(outputId = ns(paste0("sentiment", topic)), height = "100px"),
-          tags$h3("Top emotions"),
-          checkboxInput(inputId = ns(paste0("sortEmotions", topic)),
-                        label = "Sort",
-                        value = FALSE),
-          plotOutput(outputId = ns(paste0("emotion", topic)))
-        ),
-        fluidRow(
-          column(width = 2,
-            numericInput(inputId = ns(paste0("showDocuments", topic)),
-                         label = "Show top documents",
-                         value = 0, 
-                         min = 0,
-                         max = 100)
+          tags$p(paste0("Topic proportion ", round(topicProportion * 100, 0), "%"))
+        )
+      ),
+      fluidRow(
+        conditionalPanel(condition = "input.hideSentiment == false || input.hideKeywords == false", ns = ns,
+          column(width = ifelse(input$hideEmotions == FALSE, 6, 12),
+            conditionalPanel(condition = "input.hideSentiment == false", ns = ns,
+              wellPanel(
+                tags$h3("Sentiment"),
+                plotOutput(outputId = ns(paste0("sentiment", topic)), height = "100px")
+              )
+            ),
+            conditionalPanel(condition = "input.hideKeywords == false", ns = ns,
+              wellPanel(
+                tags$h3("Key words"),
+                uiOutput(outputId = ns(paste0("keywords", topic)))
+              )
+            )
           )
         ),
-        fluidRow(
-          column(width = 12,
-            conditionalPanel(condition = paste0("input.showDocuments", topic, " > 0"), ns = ns,
+        conditionalPanel(condition = "input.hideEmotions == false", ns = ns,
+          column(width= 6,
+            wellPanel(
+              tags$h3("Top emotions"),
+              plotOutput(outputId = ns(paste0("emotion", topic)))
+            )
+          )
+        )
+      ),
+      fluidRow(
+        column(width = 12,
+          conditionalPanel(condition = "input.hideDocuments == false && input.showDocuments > 0", ns = ns,
+            wellPanel(
+              tags$h3("Top documents"),
               tableOutput(outputId = ns(paste0("documents", topic)))
             )
           )
@@ -180,13 +207,14 @@ generateUI <- function(topic, model, sentiment, ns) {
 }
 
 keywordsTextFormat <- function(labels, topic) {
-  text <- paste0(
-    paste("Most probable:", paste(labels$prob[topic, ], collapse = " "), "\n"),
-    paste("Frex:", paste(labels$frex[topic, ], collapse = " "), "\n"),
-    paste("Lift:", paste(labels$lift[topic, ], collapse = " "), "\n"),
-    paste("Score:", paste(labels$score[topic, ], collapse = " "), "\n")
+  ui <- tagList(
+    tags$p(tags$strong("Most probable:"), paste(labels$prob[topic, ], collapse = " ")),
+    tags$p(tags$strong("Frex:"), paste(labels$frex[topic, ], collapse = " ")),
+    tags$p(tags$strong("Lift:"), paste(labels$lift[topic, ], collapse = " ")),
+    tags$p(tags$strong("Score:"), paste(labels$score[topic, ], collapse = " "))
   )
-  return(text)
+  
+  return(ui)
 }
 
 simpleSentimentBarPlot <- function(data, plotTitle = "Summary", plotSubtitle = "") {
